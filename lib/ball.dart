961c82eabec43a46bbb0b13526aba765d0b12b76
crystall.101600 <crystall.101600@gmail.com>
2025-02-13 17:36:45 +0000

    UI for the ball.

---
+++ lib/ball.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Ball extends StatelessWidget {
+ Ball({super.key, required this.ballX, required this.ballY});
+
+  final ballX;
+  final ballY;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment(ballX, ballY),
+      child: Container(
+        height: 15,
+        width: 15,
+        decoration: BoxDecoration(
+          color: Colors.deepPurple,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+}
