b4da2486c59fb7cfa62968a9cfad74bb2c525a1d
crystall.101600 <crystall.101600@gmail.com>
2025-02-15 17:38:42 +0000

    Amended.

--- lib/gameover_screen.dart
+++ lib/gameover_screen.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
-
 class GameoverScreen extends StatelessWidget {
-  const GameoverScreen({super.key, required this.isGameOver});
-
   final bool isGameOver;
+
+  const GameoverScreen({super.key, required this.isGameOver});
 
   @override
   Widget build(BuildContext context) {
     return isGameOver
         ? Container(
-          alignment: const Alignment(0, -0.3),
-          child: const Text(
-            'GAME OVER!', 
-            style: TextStyle(
-              color: Colors.deepPurple,
+            alignment: const Alignment(0, -0.3),
+            child: const Text(
+              'GAME OVER!',
+              style: TextStyle(
+                color: Colors.deepPurple,
+                fontSize: 32,
               ),
             ),
-        )
+          )
         : Container();
   }
 }
