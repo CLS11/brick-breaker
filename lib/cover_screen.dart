7c5e8aa0825ed50bf361576dce7d6b9c1e7ed3c4
crystall.101600 <crystall.101600@gmail.com>
2025-02-15 17:38:23 +0000

    Changed the text properties.

--- lib/cover_screen.dart
+++ lib/cover_screen.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
-
-// ignore: must_be_immutable
 class CoverScreen extends StatelessWidget {
-  bool hasGameStarted;
-  CoverScreen({super.key, required this.hasGameStarted});
+  final bool hasGameStarted;
+
+  const CoverScreen({super.key, required this.hasGameStarted});
 
   @override
   Widget build(BuildContext context) {
     return hasGameStarted
         ? Container()
         : Container(
-          alignment: Alignment(0, -0.2),
-          child: Text(
-            'Tap to play',
-            style: TextStyle(
-              color: Colors.deepPurple[400],
+            alignment: const Alignment(0, -0.2),
+            child: const Text(
+              'Tap to Play',
+              style: TextStyle(
+                color: Colors.deepPurple,
+                fontSize: 24,
+              ),
             ),
-          ),
-        );
+          );
   }
 }
