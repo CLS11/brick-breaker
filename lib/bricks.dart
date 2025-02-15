81ce5a9209201cd3d46b886a2ef9df8952c67068
crystall.101600 <crystall.101600@gmail.com>
2025-02-15 17:38:05 +0000

    Amended.

--- lib/bricks.dart
+++ lib/bricks.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-
 class Bricks extends StatelessWidget {
+  final double brickHeight;
+  final double brickWidth;
+  final double brickX;
+  final double brickY;
+  final bool brickBroken;
+
   const Bricks({
     super.key,
     required this.brickHeight,
@@ -9,26 +14,21 @@ class Bricks extends StatelessWidget {
     required this.brickY,
     required this.brickBroken,
   });
-  final double brickHeight;
-  final double brickWidth;
-  final double brickX;
-  final double brickY;
-  final bool brickBroken;
 
   @override
   Widget build(BuildContext context) {
     return brickBroken
         ? Container()
         : Container(
-          alignment: Alignment(brickX, brickY),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(5),
-            child: Container(
-              height: MediaQuery.of(context).size.height * brickHeight / 2,
-              width: MediaQuery.of(context).size.width * brickWidth / 2,
-              color: Colors.deepPurple,
+            alignment: Alignment(brickX * 2, brickY * 2),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: Container(
+                height: MediaQuery.of(context).size.height * brickHeight / 2,
+                width: MediaQuery.of(context).size.width * brickWidth / 2,
+                color: Colors.deepPurple,
+              ),
             ),
-          ),
-        );
+          );
   }
 }
